8857bcde9c2f5354de3e98f254590f54b5862b7d
Jae Kim <byungjae91@gmail.com>
2021-03-02 03:26:44 +0000

    updated path

--- toolbox/lbfgs_transfer.py
+++ toolbox/lbfgs_transfer.py
@@ -13,6 +13,7 @@ import torchvision.models as models
 
 import copy
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # desired size of the output image
 imsize = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
